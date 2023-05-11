5c33253d2c5f355d70a4143eb075c205ee77f8a1
Efim-Kapliy <efim0424@mail.ru>
2023-05-11 19:07:33 +0300

    Adding section .modal on jQuery

--- src/js/script.js
+++ src/js/script.js
@@ -23,6 +23,22 @@ $(document).ready(function () {
 
   toggleSlide(".catalog-item__link");
   toggleSlide(".catalog-item__back");
+
+  // Modal
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn(333);
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #order, #thanks").fadeOut(333);
+  });
+
+  $(".button_buy").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__description").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn(333);
+    });
+  });
 });
 
 const slider = tns({
