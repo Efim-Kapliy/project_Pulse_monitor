59a169035edc6f1497dc39272488bebfe4bb4061
Efim-Kapliy <efim0424@mail.ru>
2023-05-19 16:19:08 +0300

    Add favicon + fixing bugs + file optimization + gulp refinement

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,18 @@ const sass = require("gulp-sass")(require("sass"));
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const htmlmin = require("gulp-htmlmin");
 
 // Static server
 gulp.task("server", function () {
   browserSync.init({
     server: {
-      baseDir: "src",
+      baseDir: "dist",
     },
   });
+
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task("styles", function () {
@@ -26,13 +30,43 @@ gulp.task("styles", function () {
     )
     .pipe(autoprefixer())
     .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function () {
-  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
-  gulp.watch("src/*.html").on("change", browserSync.reload);
+  gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
+  gulp.watch("src/*.html").on("change", gulp.parallel("html"));
 });
 
-gulp.task("default", gulp.parallel("watch", "server", "styles"));
+gulp.task("html", function () {
+  return gulp
+    .src("src/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("scripts", function () {
+  return gulp.src("src/js/**/*.js").pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("fonts", function () {
+  return gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task("icons", function () {
+  return gulp.src("src/icons/**/*").pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task("mailer", function () {
+  return gulp.src("src/mailer/**/*").pipe(gulp.dest("dist/mailer"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("src/img/**/*").pipe(imagemin()).pipe(gulp.dest("dist/img"));
+});
+
+gulp.task(
+  "default",
+  gulp.parallel("watch", "server", "styles", "scripts", "fonts", "icons", "mailer", "html", "images")
+);
